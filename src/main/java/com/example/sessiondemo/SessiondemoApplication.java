7270e210b5af595b82c30ad56a9cda306b2abc9d
ttmttm <ttmttm@users.noreply.github.com>
2018-02-12 16:42:19 -0500

    init version

---
+++ src/main/java/com/example/sessiondemo/SessiondemoApplication.java
@@ -0,0 +1,12 @@
+package com.example.sessiondemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SessiondemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SessiondemoApplication.class, args);
+	}
+}
